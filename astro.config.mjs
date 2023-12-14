6d33ed2e032c624217ea6dcbeefa15dc7e0e5fd4
Belivl <mich.jelin1@gmail.com>
2023-12-14 14:05:51 +0100

    Remove i18n index fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://belivl-vercel.vercel.app/en',
+  site: 'https://belivl-vercel.vercel.app',
   integrations: [mdx(), sitemap(), tailwind()],
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "pl"],
-    routing: {
-        prefixDefaultLocale: true
-    }
-  }
+  output: "hybrid",
+  adapter: vercel()
 });
