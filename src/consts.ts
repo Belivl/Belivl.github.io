f5110d646d842ef8fcadc26b31426dcdcc447caa
Belivl <mich.jelin1@gmail.com>
2024-01-02 14:39:11 +0100

    Consts

--- src/consts.ts
+++ src/consts.ts
@@ -2,12 +2,18 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Website metadata
-export const SITE_URL: string = "https://astrostarter.zank.studio";
-export const SITE_TITLE: string = "Astro Blog";
+export const SITE_URL: string = "https://belivl-vercel.vercel.app";
+export const SITE_TITLE: string = "Belial Portfolio";
 export const SITE_DESCRIPTION: string = "Welcome to my website!";
 
 // SEO metadata
 export const TWITTER_CREATOR: string = "@xxx";
+
+
+// Misc
+export const NAME1: string = "Belial";
+export const NAME2: string = "Michal Jelinski";
+
 
 // Navigation
 type Page = {
@@ -19,17 +25,71 @@ type Page = {
 export const PAGES: Page[] = [
 	{
 		title: "Home",
-		href: "/",
+		href: "/home/",
 	},
 	{
-		title: "Blog",
-		href: "/blog",
+		title: "Portfolio",
+		href: "/portfolio/",
 	},
 	{
 		title: "About",
-		href: "/about",
+		href: "/about/",
+	},
+	{
+		title: "Contact",
+		href: "/contact/",
 	},
 ];
+// Links
+type Link = {
+	title: string;
+	href: string;
+	icon?: HTMLImageElement;
+	children?: Link[];
+	
+};
+
+export const LINKS: Link[] = [
+	{
+		title: "Instagram",
+		href: "https://www.instagram.com/belialdesign/",
+	},
+	{
+		title: "Youtube",
+		href: "https://www.youtube.com/@BelialDesign/videos",
+	},
+	{
+		title: "Behance",
+		href: "https://www.behance.net/michajeliski1",
+	},
+];
+
+// Headlines
+type Headlines = {
+	heading: string;
+	subtitle: string;
+	children?: Headlines[];
+};
+export const Headlines: Headlines[] = [
+	{
+		heading: "Portfolio",
+		subtitle: "Projects",
+	},
+	{
+		heading: "Portfolio",
+		subtitle: "Design",
+	},
+	{
+		heading: "Portfolio",
+		subtitle: "Art",
+	},
+	{
+		heading: "Portfolio",
+		subtitle: "3D",
+	},
+	
+];
+
 
 // i18n
 export const DEFAULT_LOCALE = "en";
