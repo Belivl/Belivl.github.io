7ea1c84a866f7f141eabab044b15f1bf550ce740
Belivl <mich.jelin1@gmail.com>
2024-02-03 17:12:49 +0100

    03-02 fixed theme to const dark

--- src/js/themeChange.js
+++ src/js/themeChange.js
@@ -3,7 +3,7 @@ const getThemePreference = () => {
 		if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
 			return localStorage.getItem('theme');
 		}
-		return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+		return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'dark';
 	};
 	const isDark = getThemePreference() === 'dark';
 	document.documentElement.classList[isDark ? 'add' : 'remove']('dark');
@@ -11,7 +11,7 @@ const getThemePreference = () => {
 	if (typeof localStorage !== 'undefined') {
 		const observer = new MutationObserver(() => {
 			const isDark = document.documentElement.classList.contains('dark');
-			localStorage.setItem('theme', isDark ? 'dark' : 'light');
+			localStorage.setItem('theme', isDark ? 'dark' : 'dark');
 		});
 		observer.observe(document.documentElement, { attributes: true, attributeFilter: ['class'] });
 	}
