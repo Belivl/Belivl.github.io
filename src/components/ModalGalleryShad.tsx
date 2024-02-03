c8b5425b8cde0746a19fb834c64a4003c2702342
Belivl <mich.jelin1@gmail.com>
2024-02-03 14:07:32 +0100

    03-02 fixed post @rc

--- src/components/ModalGalleryShad.tsx
+++ src/components/ModalGalleryShad.tsx
@@ -7,7 +7,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@src/components/ui/dialog";
+} from "../components/ui/dialog";
 import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
 
